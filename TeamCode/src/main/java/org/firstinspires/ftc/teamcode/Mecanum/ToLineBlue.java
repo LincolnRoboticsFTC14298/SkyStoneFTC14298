252886f78c5e2938780180aa85c13a68e78e99d3
Jared Haertel <jared.haertel@gmail.com>
2020-01-31 17:12:19 -0800

    Added toLine program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/ToLineBlue.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Mecanum;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+// Robot starts with claws facing the skyblocks and intake facing away from bridge
+@Autonomous(name="Mecanum Encoder  To Line", group="Mecanum Op")
+//@Disabled
+public class ToLineBlue extends LinearOpMode {
+    private MecanumEncoderBot robot   = new MecanumEncoderBot();   // Use a Mecanum Encoder Bot's hardware
+    private static final double     DRIVE_SPEED             = 0.3;
+    private static final double     TURN_SPEED              = 0.5;
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        waitForStart();
+        robot.strafe(DRIVE_SPEED, -25, 5, opModeIsActive());
+        robot.straight(DRIVE_SPEED, 33, 5, opModeIsActive());
+
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/ToLineRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum/ToLineRed.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 // Robot starts with claws facing the skyblocks and intake facing away from bridge
 @Autonomous(name="Mecanum Encoder  To Line", group="Mecanum Op")
 //@Disabled
-public class ToLine extends LinearOpMode {
+public class ToLineRed extends LinearOpMode {
     private MecanumEncoderBot robot   = new MecanumEncoderBot();   // Use a Mecanum Encoder Bot's hardware
     private static final double     DRIVE_SPEED             = 0.3;
     private static final double     TURN_SPEED              = 0.5;
